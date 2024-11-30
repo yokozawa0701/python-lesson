0825a9725e797c95ee7e72d8172364f93927f81e
yokozawa0701 <d.yoko.0701@gmail.com>
2024-11-30 19:02:41 +0700

    6

--- janken.py
+++ janken.py
@@ -2,8 +2,10 @@ print('じゃんけんスタート')
 print('自分の手を入力してください')
 
 my_hand = 0
-enemy_hand = 0
+enemy_hand = 1
 
 if my_hand == 0:
   if enemy_hand == 0:
     print('あいこ')
+  elif enemy_hand == 1:
+    print('勝ち')
