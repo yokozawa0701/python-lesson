eb1221644c5fbffdebf286135e47ec24c6f5b5a9
yokozawa0701 <d.yoko.0701@gmail.com>
2024-11-30 19:27:32 +0700

    自分の手, 相手の手を出すように

--- janken.py
+++ janken.py
@@ -1,16 +1,24 @@
 import random
 from datetime import date
 
+def hand_name(hand):
+    return ['グー', 'チョキ', 'パー'][hand]
+
 def start_message():
   print(date.today())
   print('じゃんけんスタート')
 
 def get_my_hand():
   print('自分の手を入力してください')
-  return int(input('0:グー, 1:チョキ, 2:パー'))
+  hand = int(input('0:グー, 1:チョキ, 2:パー'))
+  print('あなたの手は{}です'.format(hand_name(hand)))
+
+  return hand
 
 def get_you_hand():
-  return random.randint(0, 2)
+  hand = random.randint(0, 2)
+  print('相手の手は{}です'.format(hand_name(hand)))
+  return hand
 
 def view_result(my_hand, enemy_hand):
   result = my_hand - enemy_hand
